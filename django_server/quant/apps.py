1a19a7ff8a786868d8b70b08ef080c93c2b81ce9
Af7007 <linetecnologia@gmail.com>
2025-10-21 19:54:44 -0300

    \"Remove Django server completely\"

--- django_server/quant/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuantConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quant'
-    verbose_name = 'Sistema MLP Quantitativo'
